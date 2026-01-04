43bba75fe0c39660db3526fa68125ca07310bf22
Ashish kumar <ashish789504@gmail.com>
2026-01-04 15:07:16 +0530

    fourth commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken'
 
 export const generateToken=(userId,res)=>{
- const token=jwt.sign({userId},process.env.jwt_SECRET,{
+   if(!process.env.JWT_SECRET){
+      throw new Error("jwt_secret is missing")
+   }
+ const token=jwt.sign({userId},process.env.JWT_SECRET,{
     expiresIn:'7d'
  })
  res.cookie("jwt",token,{maxAge:7*24*60*60*1000,
